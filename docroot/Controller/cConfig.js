a9b21b3dc0014a2f92e23dbdf8b66b4e5d1efc31
GJ-DARIS <daris@goingjesse.com>
2018-07-14 18:46:09 +0700

    Commit report-kpis

--- docroot/Controller/cConfig.js
+++ docroot/Controller/cConfig.js
@@ -2,17 +2,20 @@ var restfulURL="";
 var serviceName="see_api";
 
 var serviceName_IMPkpi="impexpservices_api/public";
-var restfulURL_IMPkpi="https://tfg-winner.tfg.co.th:8443";
+//var restfulURL_IMPkpi="https://tfg-winner.tfg.co.th:8443";
+var restfulURL_IMPkpi="http://localhost";
 /*#######Office#######*/
 
 //restfulURL="http://192.168.1.254";
 
 /*#######Localhost#######*/
-restfulURL="https://tfg-winner.tfg.co.th:8443";
+//restfulURL="https://tfg-winner.tfg.co.th:8443";
+restfulURL="http://localhost";
 
 
 /*#######Localhost#######*/
-var lifeRayApiUrl = "https://tfg-winner.tfg.co.th";
+//var lifeRayApiUrl = "https://tfg-winner.tfg.co.th";
+var lifeRayApiUrl = "http://localhost";
 
 /*#######Poroduction#######*/
 //restfulURL="http://172.16.11.109";
