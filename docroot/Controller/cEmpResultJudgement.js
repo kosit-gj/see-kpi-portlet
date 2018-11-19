13f819d74b1a61197b3937fbf798f05f99debe63
Wirun Pengsri <wirun@goingjesse.com>
2018-11-19 17:50:40 +0700

    [Edited]: Parameter Position

--- docroot/Controller/cEmpResultJudgement.js
+++ docroot/Controller/cEmpResultJudgement.js
@@ -146,7 +146,10 @@ var dropDrowPositionFn = function(id){
 		dataType:"json",
 		async:false,
 		headers:{Authorization:"Bearer "+tokenID.token},
-		//data:{"Position":Position},
+		data:{
+			"organization_id" : $("#organization").val(),
+			"employee_id" : $("#EmpName_id").val()
+		},
 		success:function(data){
 			var htmlOption="";
 			$.each(data,function(index,indexEntry){
@@ -588,6 +591,7 @@ $(document).ready(function() {
 				appraisalStatusFn();
 			});
 			
+			
 			$("#Position").multiselect({minWidth:'100%;'}).multiselectfilter();
 			  refreshMultiPosition();
 			
@@ -637,8 +641,16 @@ $(document).ready(function() {
 					} else {
 						$("#EmpName_id").val("");
 					}
+					
+					dropDrowPositionFn();
+					refreshMultiPosition();
 		         }       
 		    });
+			
+			$("#organization").change(function() {
+				dropDrowPositionFn();
+				refreshMultiPosition();
+			});
 			
 			//Search Start
 		    $("#btnSearchAdvance").click(function () {
