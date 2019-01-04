e64978cf2add01e76a621babfca9ccc113d924b3
GJ-Chokanan <chokanan@goingjesse.com>
2019-01-04 15:36:52 +0700

    [Edited]: Autoconfiguration ip address service default port ถ้า liferay ใช้ protocol HTTPS ตัว Service จะต้องตั้งค่า PORT เป็น 8443 ถ้า liferay ใช้ protocol HTTP ตัว Service จะต้องตั้งค่า PORT เป็น 80

--- docroot/Controller/cConfig.js
+++ docroot/Controller/cConfig.js
@@ -1,21 +1,34 @@
 var restfulURL="";
+var restfulURL_IMPkpi="";
+var lifeRayApiUrl="";
 var serviceName="see_api_fmo";
-
 var serviceName_IMPkpi="impexpservices_api/public";
+var servicePort = (document.location.protocol == "https:" ? ":8443" : ""); // HTTP default port :80
+
+	restfulURL = document.location.protocol + "//" + document.location.hostname + servicePort ;
+	restfulURL_IMPkpi = document.location.protocol + "//" + document.location.hostname + servicePort ;
+	lifeRayApiUrl= document.location.origin;
+
+
+
+//var restfulURL="";
+//var serviceName="see_api_fmo";
+
+//var serviceName_IMPkpi="impexpservices_api/public";
 //var restfulURL_IMPkpi="https://tfg-winner.tfg.co.th:8443";
-var restfulURL_IMPkpi="http://192.168.1.42";
+//var restfulURL_IMPkpi="http://192.168.1.42";
 /*#######Office#######*/
 
 //restfulURL="http://192.168.1.254";
 
 /*#######192.168.1.42#######*/
 //restfulURL="https://tfg-winner.tfg.co.th:8443";
-restfulURL="http://192.168.1.42";
+//restfulURL="http://192.168.1.42";
 
 
 /*#######192.168.1.42#######*/
 //var lifeRayApiUrl = "https://tfg-winner.tfg.co.th";
-var lifeRayApiUrl = "http://192.168.1.42:8080";
+//var lifeRayApiUrl = "http://192.168.1.42:8080";
 
 /*#######Poroduction#######*/
 //restfulURL="http://172.16.11.109";
