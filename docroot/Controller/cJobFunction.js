bb737b08d11dadbe82e86c441105e7dcbf7daab4
GJ-Chokanan <chokanan@goingjesse.com>
2019-03-25 17:41:27 +0700

    [Edited]: (Defect.402) Developing new screen to maintain job function master data

--- docroot/Controller/cJobFunction.js
+++ docroot/Controller/cJobFunction.js
@@ -11,37 +11,39 @@ $(document).ready(function(){
 	 		
     	var options={
     			"colunms":[
-    						{"colunmsDisplayName":"Position Code","width":"15%","id":"position_code","colunmsType":"text"},
-    			           {"colunmsDisplayName":"Position Name","width":"60%","id":"position_name","colunmsType":"text"},
-    			           {"colunmsDisplayName":"Is Active","width":"auto","id":"is_active","colunmsType":"checkbox"},
+    						{"colunmsDisplayName":"Job Function Name","width":"60%","id":"job_function_name","colunmsType":"text"},
+    			           {"colunmsDisplayName":"Is Evaluated","width":"auto","id":"is_evaluated","colunmsType":"checkbox"},
+    			           {"colunmsDisplayName":"Is Headcount","width":"auto","id":"is_headcount","colunmsType":"checkbox"},
     			          ],
     			"form":[{
-						"label":"Position Code","inputType":"text","placeholder":"Position Code",
-						"id":"position_code","width":"250px","required":true
-						},{
-    					"label":"Position Name","inputType":"text","placeholder":"Position Name",
-    					"id":"position_name","width":"350px","required":true
-    					},
+						"label":"Job Function Name","inputType":"text","placeholder":"Job Function Name",
+						"id":"job_function_name","width":"250px","required":true
+						},
     					{
-	 	    			"label":"IsActive","inputType":"checkbox","default":"checked",
-	 	    			"id":"is_active","width":"200px"
-	 	    			}
+	 	    			"label":"Is Evaluated","inputType":"checkbox","default":"",
+	 	    			"id":"is_evaluated","width":"200px"
+	 	    			},
+    					{
+		 	    			"label":"Is Headcount","inputType":"checkbox","default":"",
+		 	    			"id":"is_headcount","width":"200px"
+		 	    			}
     					
     			     ], 	
     			 "advanceSearch":[{
-     	 				"label":"Position","label_tooltip":"Position","inputType":"autoComplete","placeholder":"Position",
-     	 				"id":"position_name","width":"100%","default":""
+    				 	"label":"Job Function","label_tooltip":"Job Function","inputType":"dropdown",
+    					"id":"job_function_id","width":"100%","initValue":"All Job Function",
+    					"url":""+restfulURL+"/"+serviceName+"/public/job_function/list"
      				    }
      				    ],
-    			 "formDetail":{"formSize":"modal-dialog","formName":"Position","id":"position","pk_id":"position_id"},       
-    			 "serviceName":[restfulURL+"/"+serviceName+"/public/position"],
+    			 "formDetail":{"formSize":"modal-dialog","formName":"Fob Function","id":"job_function","pk_id":"job_function_id"},       
+    			 "serviceName":[restfulURL+"/"+serviceName+"/public/job_function"],
     			 "tokenID":tokenID,
     			 "pagignation":false,
     			 "expressSearch":false,
     			 "advanceSearchSet":true,
-    			 "btnAddOption":false,
-    			 "btnAdvanceDownloadOption":{"url":""+$("#url_portlet").val()+"/file/appraisal_position_template.xlsx"},
-    			 "btnAdvanceImportOption":{"formName":"Import Position","accept":".xls ,.xlsx"}
+    			 "btnAddOption":true,
+    			 //"btnAdvanceDownloadOption":{"url":""+$("#url_portlet").val()+"/file/appraisal_position_template.xlsx"},
+    			 //"btnAdvanceImportOption":{"formName":"Import Position","accept":".xls ,.xlsx"}
     			 //"btnManageOption":{"id":"BtnID","name":"BtnName"},
     			 //"btnAdvanceSearchOption":{"id":"BtnID","name":"<i class=\"fa fa-plus-square\"></i>&nbsp;Btn"}
     	}
@@ -49,47 +51,8 @@ $(document).ready(function(){
     	createDataTableFn(options);
     	
 		}
-	 	//Autocomplete From Position Start
-    	$("form#searchAdvanceForm #position_name input").autocomplete({
-            source: function (request, response) {
-            	$.ajax({
-    				 url:restfulURL+"/"+serviceName+"/public/position/auto",
-    				 type:"POST",
-    				 dataType:"json",
-    				 data:{
-    					 "q":request.term},
-    				//async:false,
-    				 headers:{Authorization:"Bearer "+tokenID.token},
-                     error: function (xhr, textStatus, errorThrown) {
-                            console.log('Error: ' + xhr.responseText);
-                        },
-    				 success:function(data){
-    					  
-    						response($.map(data, function (item) {
-    							var dataSet = new Object();
-    							//autocomplete default values REQUIRED
-    							dataSet.label = item.position_name;
-    							dataSet.value = item.position_name;
-
-                                //extend values
-    							dataSet.position_id = item.position_id;
-
-    							console.log(dataSet);
-                                return dataSet;
-                            }));
-    					
-    				},
-    				beforeSend:function(){
-    					$("body").mLoading('hide');	
-    				}
-    				
-    				});
-            }
-        });
+	 	
        
-    	//Autocomplete From Position End
-    	
-    	$("#position_code").prop("disabled", true);
 	 }
     });
  
